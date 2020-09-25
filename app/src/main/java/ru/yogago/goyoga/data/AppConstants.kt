8e6066f98208ab38310c95ef2bef0aeebe5b8248
Mikhail Shkuro <white.electric.dog@gmail.com>
2020-09-25 12:03:06 +0300

    translate

--- app/src/main/java/ru/yogago/goyoga/data/AppConstants.kt
+++ app/src/main/java/ru/yogago/goyoga/data/AppConstants.kt
@@ -1,7 +1,6 @@
 package ru.yogago.goyoga.data
 
 object AppConstants{
-    val levels = arrayOf("Не указан", "Подготовительный", "Начальный 1", "Начальный 2", "Основной")
     const val BASE_URL = "https://yoga-go.ru/"
     const val PHOTO_URL = "https://yoga-go.ru/asana/"
     const val LOG_TAG: String = "myLog"
