58361147d55dae7519f7e7fab87e998c4a669846
Mikhail Shkuro <white.electric.dog@gmail.com>
2020-10-07 17:27:58 +0300

    remove login and custom registration

--- app/src/main/java/ru/yogago/goyoga/data/LoginResult.kt
+++
@@ -1,9 +0,0 @@
-package ru.yogago.goyoga.data
-
-/**
- * Authentication result : success (user details) or error message.
- */
-data class LoginResult(
-    val success: String? = null,
-    val error: String? = null
-)
