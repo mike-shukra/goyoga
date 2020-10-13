b552d3619d537b03393b6df3af85c85d04af7a77
Mikhail Shkuro <white.electric.dog@gmail.com>
2020-10-13 14:47:05 +0300

    Yandex AppMetrica

--- app/src/main/java/ru/yogago/goyoga/data/AppConstants.kt
+++ app/src/main/java/ru/yogago/goyoga/data/AppConstants.kt
@@ -8,6 +8,7 @@ class AppConstants {
         const val LOG_TAG: String = "myLog"
         const val LOG_TAG_BILLING: String = "billingLog"
         const val APP_TOKEN = "9aHKJkgjk9ajhuiK53DF7683hy35sgSjkasd3898jkasd8LHGf9"
+        const val API_key = "6e39ab29-3de3-4c18-88fc-ef061f56f4d6"
         const val PLAY_STORE_SUBSCRIPTION_URL =
             "https://play.google.com/store/account/subscriptions"
         const val PLAY_STORE_SUBSCRIPTION_DEEPLINK_URL =
