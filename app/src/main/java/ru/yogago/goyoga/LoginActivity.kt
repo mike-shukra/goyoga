402971592bfe49c03324ecac9981cd77d9cd8b42
79650 <white.electric.dog@gmail.com>
2023-09-10 19:47:24 +0700

    firebase auth

--- app/src/main/java/ru/yogago/goyoga/LoginActivity.kt
+++ app/src/main/java/ru/yogago/goyoga/LoginActivity.kt
@@ -3,25 +3,34 @@ package ru.yogago.goyoga
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
 import ru.yogago.goyoga.data.AppConstants
 
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var tvRedirectSignUp: TextView
-    lateinit var etEmail: EditText
+    private lateinit var etEmail: EditText
     private lateinit var etPass: EditText
-    lateinit var btnLogin: Button
-
+    private lateinit var btnLogin: Button
+    private lateinit var btnSignInGoogle: Button
+    private val Req_Code: Int = 123
+    private lateinit var mGoogleSignInClient: GoogleSignInClient
     // Creating firebaseAuth object
-    lateinit var auth: FirebaseAuth
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,6 +39,7 @@ class LoginActivity : AppCompatActivity() {
         // View Binding
         tvRedirectSignUp = findViewById(R.id.tvRedirectSignUp)
         btnLogin = findViewById(R.id.btnLogin)
+        btnSignInGoogle = findViewById(R.id.btnSignInGoogle)
         etEmail = findViewById(R.id.etEmailAddress)
         etPass = findViewById(R.id.etPassword)
 
@@ -44,6 +54,73 @@ class LoginActivity : AppCompatActivity() {
             val intent = Intent(this, SignUpActivity::class.java)
             startActivity(intent)
             // using finish() to end the activity
+            finish()
+        }
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        btnSignInGoogle.setOnClickListener { view: View? ->
+            Toast.makeText(this, "Logging In", Toast.LENGTH_SHORT).show()
+            getToken()
+            signInGoogle()
+        }
+
+//        getToken()
+    }
+
+    private fun signInGoogle() {
+        val signInIntent: Intent = mGoogleSignInClient.signInIntent
+        startActivityForResult(signInIntent, Req_Code)
+    }
+
+    // onActivityResult() function : this is where
+    // we provide the task and data for the Google Account
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == Req_Code) {
+            val task: Task<GoogleSignInAccount> = GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleResult(task)
+        }
+    }
+
+    private fun handleResult(completedTask: Task<GoogleSignInAccount>) {
+        try {
+            val account: GoogleSignInAccount? = completedTask.getResult(ApiException::class.java)
+            if (account != null) {
+                UpdateUI(account)
+            }
+        } catch (e: ApiException) {
+            Toast.makeText(this, e.toString(), Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    // this is where we update the UI after Google signin takes place
+    private fun UpdateUI(account: GoogleSignInAccount) {
+        val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+        auth.signInWithCredential(credential).addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+//                SavedPreference.setEmail(this, account.email.toString())
+//                SavedPreference.setUsername(this, account.displayName.toString())
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if (GoogleSignIn.getLastSignedInAccount(this) != null) {
+            startActivity(
+                Intent(
+                    this, MainActivity
+                    ::class.java
+                )
+            )
             finish()
         }
     }
@@ -62,6 +139,7 @@ class LoginActivity : AppCompatActivity() {
                 startActivity(intent)
                 // using finish() to end the activity
                 finish()
+                // ...
 
             } else
                 Toast.makeText(this, "Log In failed ", Toast.LENGTH_SHORT).show()
@@ -69,14 +147,14 @@ class LoginActivity : AppCompatActivity() {
     }
 
     private fun getToken() {
-        val mUser = FirebaseAuth.getInstance().currentUser
+        val mUser = auth.currentUser
         mUser!!.getIdToken(true)
             .addOnCompleteListener { task ->
                 if (task.isSuccessful) {
                     val idToken: String? = task.result.token
                     // Send token to your backend via HTTPS
                     Log.d(AppConstants.LOG_TAG, "token: $idToken")
-                    // ...
+
                 } else {
                     // Handle error -> task.getException();
                 }
--- app/src/main/java/ru/yogago/goyoga/MainActivity.kt
+++ app/src/main/java/ru/yogago/goyoga/MainActivity.kt
@@ -1,8 +1,11 @@
 package ru.yogago.goyoga
 
+import android.content.Intent
 import android.content.res.Configuration
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -11,7 +14,11 @@ import androidx.navigation.ui.setupWithNavController
 import com.google.android.gms.auth.api.identity.BeginSignInRequest
 import com.google.android.gms.auth.api.identity.Identity
 import com.google.android.gms.auth.api.identity.SignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.auth.FirebaseAuth
 import java.util.*
 
 
@@ -19,6 +26,13 @@ class MainActivity : AppCompatActivity() {
 
 //    private lateinit var oneTapClient: SignInClient
 //    private lateinit var signInRequest: BeginSignInRequest
+
+    // declare the GoogleSignInClient
+    private lateinit var mGoogleSignInClient: GoogleSignInClient
+
+    private val auth by lazy {
+        FirebaseAuth.getInstance()
+    }
 
     companion object {
         var dLocale: Locale = Locale("")
@@ -38,6 +52,11 @@ class MainActivity : AppCompatActivity() {
         supportActionBar?.hide()
 
         setContentView(R.layout.activity_main)
+//        val logOutButton = this.findViewById<Button>(R.id.btnLogout)
+//        logOutButton.setOnClickListener {
+//            logOut()
+//        }
+
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
 //        oneTapClient = Identity.getSignInClient(this)
@@ -69,6 +88,25 @@ class MainActivity : AppCompatActivity() {
         navView.setupWithNavController(navController)
 
         if (isLandSpace()) navView.visibility = View.GONE
+
+        // call requestIdToken as follows
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+
+
+    }
+
+    fun logOut() {
+        mGoogleSignInClient.signOut().addOnCompleteListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            Toast.makeText(this, "Logging Out", Toast.LENGTH_SHORT).show()
+            startActivity(intent)
+            finish()
+        }
+
     }
 
     private fun isLandSpace(): Boolean {
